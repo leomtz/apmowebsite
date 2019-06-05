bbe3ecd88ed066ee9a54b1bfe37ca770f47e3644
Leonardo Martínez <ssbmplayer@gmail.com>
2019-06-05 18:53:04 +0200

    Added first version of timeline view

--- webpage.py
+++ webpage.py
@@ -6,6 +6,27 @@ year_re=r'[0-9]{4}'
 
 app = Flask(__name__)
 
+# An auxiliary function for creating a table
+
+def country_year_table(code,year): 
+    year_res_df = pd.read_csv('data/reports/score_awards_%s.csv' % year)
+    to_report = year_res_df[year_res_df.code == code].reset_index().drop('index', axis=1)
+    country=to_report['country'][0]
+    to_report.drop(['country', 'sex', 'code'], axis=1, inplace=True)
+    to_report.columns=["Rank", "Last Name", "First Name", "P1", "P2", "P3", "P4", "P5", "Total", "Award"]
+    table=to_report.to_html(index=False, classes='table table-striped table-sm',border=0,justify='center')
+    return table, country
+
+def apmo_editions():
+    editions_df = pd.read_csv('data/apmo_editions.csv').drop("chair", axis=1)
+    editions_df.columns=['Year', 'SCC', 'ACC', 'MC']
+    editions_df.sort_values('Year', ascending=False,inplace=True)
+    editions_df['Results']='<a href=year_report/' + editions_df.Year.astype(str) +'> Go </a>'
+    table=editions_df.to_html(index=False, classes='table table-striped table-sm', border=0, justify='center',na_rep='', escape=False)
+    return table
+
+# The routing begins here
+
 @app.route('/')
 def hello_world():
     return render_template("landing.html")
@@ -20,7 +41,8 @@ def regulations():
 
 @app.route('/timeline')
 def timeline():
-    return render_template("timeline.html")
+    table=apmo_editions()
+    return render_template("timeline.html", table=table)
 
 @app.route('/countries')
 def countries():
@@ -52,15 +74,12 @@ def year(year):
         competition_info = json.loads(json_file.read())
     return render_template("year_report.html",year=year, by_country=by_country, competition_info=competition_info)
 
-@app.route('/country_report/<country>/<year>')
-def country(country, year):
+@app.route('/country_report/<code>/<year>')
+def country(code, year):
     if year=='all':
         pass
-    if int(year)>=2016:
+    if int(year)<=2015:
         pass
-    with open('static/data/by_country_ranked_%s.html' % year, 'r') as file:
-        by_country=file.read()
-    with open('static/data/apmo_%s_info.json' % year) as json_file:
-        competition_info = json.loads(json_file.read())
-
-    return render_template("year_report.html",year=year, by_country=by_country, competition_info=competition_info)
+    if int(year) in range(2016,2020):
+        table, country = country_year_table(code, year)
+        return render_template('year_country_report.html', code=code, year=year, country=country, table=table)
